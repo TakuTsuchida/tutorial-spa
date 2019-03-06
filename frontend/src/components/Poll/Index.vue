2d207b947bc5dadad4d9bbb6d20802c8839720e3
土田拓 <takutsuchida@tsuchitatakunoMacBook-Pro.local>
2019-03-06 19:47:25 +0900

    tutorial-spa/03/cros

---
+++ frontend/src/components/Poll/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    polls
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'PollIndex',
+  methods: {
+    fetchData () {
+      axios.get('http://localhost:8000/api/1.0/questions/').then(res => {
+        console.log(res)
+      })
+    },
+  },
+  mounted () {
+    this.fetchData()
+  },
+}
+</script>
+
+<style>
+</style>
