fbf3895167c63c39429b79f5d79cefb5cfca3e69
土田拓 <takutsuchida@tsuchitatakunoMacBook-Pro.local>
2019-03-06 23:29:01 +0900

    tutorial-spa/04/display selector

--- frontend/src/components/Poll/Index.vue
+++ frontend/src/components/Poll/Index.vue
@@ -8,8 +8,19 @@
               <v-card-title primary-title>
                 <div>{{ data.questionText }}</div>
               </v-card-title>
+              <v-card-text v-if="data.choices.length">
+                <v-radio-group v-model="vote">
+                  <v-radio
+                    v-for="choice in data.choices"
+                    :key="choice.id"
+                    :label="choice.choiceText + '投票数' + choice.votes"
+                    :value="choice.id">
+                  </v-radio>
+                </v-radio-group>
+                <v-btn color="success">投票</v-btn>
+              </v-card-text>
               <v-card-text>
-                <div>{{ data.pubDate }}</div>
+                <div>{{ data.pubDate|printDate}}</div>
               </v-card-text>
             </v-card>
           </v-flex>
@@ -26,6 +37,7 @@ export default {
   name: 'PollIndex',
   data () {
     return {
+      vote: null,
       questions: [],
     }
   },
